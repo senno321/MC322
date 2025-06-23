69de3ba380557c94c9d5d9c66e8ff3ba19062733
Bruno Medeiros Saback <ra281746@dac.unicamp.br>
2025-06-22 23:03:37 -0300

    consertei

--- app/src/main/java/mc322/materia/GerenciadorDeMaterias.java
+++ app/src/main/java/mc322/materia/GerenciadorDeMaterias.java
@@ -53,10 +53,10 @@ public class GerenciadorDeMaterias {
      */
     private void carregarMateriasDosArquivos() {
         // Caminhos relativos dos arquivos de dados dentro de resources
-        String caminhoArquivoCodigos = "mc322/txt/Código_Matérias.txt";
-        String caminhoArquivoNomes = "mc322/txt/Nome_Matérias.txt";
+        String caminhoArquivoCodigos = "mc322/txt/Codigo_Materias.txt";
+        String caminhoArquivoNomes = "mc322/txt/Nome_Materias.txt";
         String caminhoArquivoProfessores = "mc322/txt/Nome_Professores.txt";
-        String caminhoArquivoCreditos = "mc322/txt/Crédito_Matérias.txt";
+        String caminhoArquivoCreditos = "mc322/txt/Credito_Materias.txt";
 
         List<String> codigos = lerArquivoDeTexto(caminhoArquivoCodigos);
         List<String> nomes = lerArquivoDeTexto(caminhoArquivoNomes);
